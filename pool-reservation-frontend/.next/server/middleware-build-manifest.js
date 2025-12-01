b797747c7ac7e85b1715f28e6c8d4b0460ee5b60
unknown <christianjay.lina@hcdc.edu.ph>
2025-12-01 20:03:04 +0800

    Fix API URL for production deployment

--- pool-reservation-frontend/.next/server/middleware-build-manifest.js
+++ pool-reservation-frontend/.next/server/middleware-build-manifest.js
@@ -1,1 +1,18 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-ae7f8552ea7f451c.js","static/chunks/fd9d1056-2a00d09cb9d99b82.js","static/chunks/117-3831501d0d656f14.js","static/chunks/main-app-3175efb1a5e5efc2.js"],pages:{"/_app":["static/chunks/webpack-ae7f8552ea7f451c.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-6e3f8bcef3926068.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-ae7f8552ea7f451c.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-6e3f8bcef3926068.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
